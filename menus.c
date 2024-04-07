eb58c411d17f78b5f070cb13f722661128998f0c
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-04-07 19:53:20 +0200

    solucion añadir sala

--- menus.c
+++ menus.c
@@ -140,35 +140,32 @@ void menuAnadirCine()
 
 void limpiarBuffer() {
     int c;
-    while ((c = getchar()) != '\n' && c != EOF) {
-        // Vaciar el búfer de entrada
-    }
-    if (c == EOF) {
-        // Si se alcanza el final de archivo, salir del programa
-        printf("Error: Se ha alcanzado el final del archivo.\n");
-        exit(EXIT_FAILURE);
-    }
+    printf("Presiona Enter para continuar...");
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void menuAnadirSala()
-{
+void menuAnadirSala() {
     printf("\n============\nANADIR SALA\n============\n\n");
+    
     printf("Introduce el id del cine en la que esta la sala: ");
     fgets(cine.idCineChar, 4, stdin);
     limpiarBuffer();
     sscanf(cine.idCineChar, "%i", &cine.idCineInt);
+    
     printf("Cual es el numero de filas que contiene la sala? ");
     fgets(sala.nFilasSalaChar, 3, stdin);
     limpiarBuffer();
-    sscanf(sala.nFilasSalaChar, "%i", sala.nFilasSalaInt);
+    sscanf(sala.nFilasSalaChar, "%i", &sala.nFilasSalaInt);
+    
     printf("Cual es el numero de columnas que contiene la sala? ");
     fgets(sala.nColumnasSalaChar, 3, stdin);
     limpiarBuffer();
-    sscanf(sala.nColumnasSalaChar, "%i", sala.nColumnasSalaInt);
+    sscanf(sala.nColumnasSalaChar, "%i", &sala.nColumnasSalaInt);
+    
     printf("Cual es el numero de la sala? ");
     fgets(sala.numeroSalaChar, 3, stdin);
     limpiarBuffer();
-    sscanf(sala.numeroSalaChar, "%i", sala.numeroSalaInt);
+    sscanf(sala.numeroSalaChar, "%i", &sala.numeroSalaInt);
 }
 
 void menuAnadirActor()
